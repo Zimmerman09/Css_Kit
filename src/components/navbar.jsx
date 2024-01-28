e8fd98e04c2247fd6ba0683c72df45ad545c4a49
Sahitya Kumar Choubey <sahityakumarchoubey@gmail.com>
2024-01-28 12:09:57 +0530

    added gsap animation

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,37 +1,53 @@
-import React from 'react'
-import './navbar.css'
+import React, { useLayoutEffect, useRef } from "react";
+import "./navbar.css";
+import { gsap, Elastic } from "gsap";
+import { useGSAP } from "@gsap/react";
 
 const Navbar = () => {
+  const con = useRef(null);
+  useGSAP(() => {
+    gsap.from(".text", {
+      y: 100,
+      duration: 0.5,
+      stagger: 0.05,
+      ease: Elastic.easeOut.config(0.3, 0.2)
+    });
+  }, []);
   return (
-   <>
-    <div className='container'>
-        <div className='circle1'></div>
-        <div className='circle2'></div>
+    <>
+      <div className="container">
+        <div className="circle1"></div>
+        <div className="circle2"></div>
         <nav>
-            <img src=''/>
-            
-            <ul className='ul1'>
-                <li>Home</li>
-                <li>Box-Shadow</li>
-                <li>Button</li>
-                <li>Hover</li>
-            </ul>
+          <img src="" />
 
-            <ul>
-                  <li><button className='btn1'>About Us</button></li>
-                  <li><button className='btn2'>Contact</button></li>
-            </ul>            
+          <ul className="ul1">
+            <li>Home</li>
+            <li>Box-Shadow</li>
+            <li>Button</li>
+            <li>Hover</li>
+          </ul>
+
+          <ul>
+            <li>
+              <button className="btn1">About Us</button>
+            </li>
+            <li>
+              <button className="btn2">Contact</button>
+            </li>
+          </ul>
         </nav>
-        <div className="content">
-            MAKE YOUR WEBSITES<br/> LOOK MORE <br/><span>ATTRACTIVE</span>
+        <div ref={con} className="content">
+        <div class="context"><div className="text">M</div><div className="text">A</div><div className="text">K</div><div className="text">E</div><div className="text">&nbsp;</div><div className="text">Y</div><div className="text">O</div><div className="text">U</div><div className="text">R</div><div className="text">&nbsp;</div><div className="text">W</div><div className="text">E</div><div className="text">B</div><div className="text">I</div><div className="text">T</div><div className="text">E</div><div className="text">S</div></div>
+          <div class="context"><div className="text">L</div><div className="text">O</div><div className="text">O</div><div className="text">K</div><div className="text">&nbsp;</div><div className="text">M</div><div className="text">O</div><div className="text">R</div><div className="text">E</div></div>
+          <div class="context"><div className="text" id="red-text">A</div><div className="text" id="red-text">T</div><div className="text" id="red-text">T</div><div className="text" id="red-text">R</div><div className="text" id="red-text">A</div><div className="text" id="red-text">C</div><div className="text" id="red-text">T</div><div className="text" id="red-text">I</div><div className="text" id="red-text">V</div><div className="text" id="red-text">E</div></div>
         </div>
+      </div>
+      <div className="btn3">
+        <button className="for-knowmore-btn">Know More</button>
+      </div>
+    </>
+  );
+};
 
-    </div>
-    <div className='btn3'>
-        <button className='for-knowmore-btn'>Know More</button>
-    </div>
-   </>
-  )
-}
-
-export default Navbar
+export default Navbar;
